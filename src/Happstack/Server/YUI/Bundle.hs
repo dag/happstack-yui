65cb31204b48bd908288f62424ed64ac8a2f50f7
Dag Odenhall <dag.odenhall@gmail.com>
2012-05-22 23:56:56 +0200

    make use of file-embed conditional with a cabal flag

--- src/Happstack/Server/YUI/Bundle.hs
+++ src/Happstack/Server/YUI/Bundle.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE CPP, TemplateHaskell #-}
 
 module Happstack.Server.YUI.Bundle
   ( isYUIFile
@@ -6,14 +6,31 @@ module Happstack.Server.YUI.Bundle
   ) where
 
 import Data.ByteString (ByteString)
-import Data.FileEmbed  (embedDir)
-import Data.Map        (Map, (!), fromList, member)
 
+#if EMBED
+import Data.FileEmbed            (embedDir)
+import Data.Map                  (Map, (!), fromList, member)
+#else
+import Paths_happstack_yui       (getDataFileName)
+import System.Directory          (doesFileExist)
+import qualified Data.ByteString as B
+#endif
+
+#if EMBED
 bundle :: Map FilePath ByteString
 bundle = fromList $(embedDir "bundle")
+#endif
 
 isYUIFile :: FilePath -> IO Bool
+#if EMBED
 isYUIFile name = return $ member name bundle
+#else
+isYUIFile name = getDataFileName ("bundle/" ++ name) >>= doesFileExist
+#endif
 
 readYUIFile :: FilePath -> IO ByteString
+#if EMBED
 readYUIFile name = return $ bundle ! name
+#else
+readYUIFile name = getDataFileName ("bundle/" ++ name) >>= B.readFile
+#endif
